84a7a3a305b6bcbc118edf66045d2c432b16f23f
GaroWarner <gerriew@gmail.com>
2020-11-09 06:38:16 +0100

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fa8d32f2c4ef2001e31fdad",
+				"5fa8d32f2c4ef2001e31fdaf"
+			]
+		},
+		"5fa8d32f2c4ef2001e31fdad": {
+			"id": "5fa8d32f2c4ef2001e31fdad",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fa8d32f2c4ef2001e31fdaf": {
+			"id": "5fa8d32f2c4ef2001e31fdaf",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
